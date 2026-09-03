c632472e52d97ac614052afba9e4ab136a5530bd
Alan Maguire <maguire.alan.e@gmail.com>
2026-09-03 17:33:13 +0100

    bpf: Support bpf-based symbolization for USDT probes
    USDT probes for python now pass PyThreadState as the
    final parameter; we can use it to walk the stack frames
    to display associated filename and function for each.
    
    Add an ELF section called freplace/bpf_stack to libpython
    which is a BPF program responsible for stringifying a
    python stack.
    
    It is linked into BPF programs (replacing a dummy function
    of the same name), and this allows the program to print
    python stacks.  For example the following program can
    print stacks to /sys/kernel/debug/tracing/trace_pipe
    from the "function__entry" USDT probe:
    
    SEC("usdt/libpython3.16.so:function__entry")
    int trace_function_entry(struct pt_regs *ctx)
    {
            struct bpf_stack_args args = { .depth = 16, .str_sep = '<' };
            int err;
    
            err = bpf_usdt_stack(&args);
            if (!err)
                    bpf_printk("python stack (%d bytes): %s", args.str_len, args.str);
            return 0;
    }
    
    bpf_usdt_stack() is a macro that collects the final argument
    from the USDT probe context and calls bpf_stack() with it
    in the bpf_stack_args data field.
    
    In this case libbpf USDT code would - as part of handling
    the USDT representation encoded in ELF notes - find the
    freplace/bpf_stack ELF section, and replace the dummy bpf_stack()
    function with it during load/attach.
    
    Stacks are printed in leaf-to-root order, with a user-supplied
    depth and separator between each entry.
    
    The idea is that the runtime (python3 in this case) is
    responsible for delivering a means to stringify a stack;
    the tracer then attaches it for use.
    
    It does this with the help of the bpf_stack.h header
    which provides the bpf_stack_args struct and some helper
    functions to append strings and characters to the stack.
    The remaining work is to take the PyThreadState and
    walk the stack frames associated with it, passing the
    recovered strings to those helpers to construct the
    stack.
    
    Signed-off-by: Alan Maguire <maguire.alan.e@gmail.com>

---
+++ Python/bpf/bpf_stack.bpf.c
@@ -0,0 +1,105 @@
+/*
+ * CPython BPF stack unwinder.
+ *
+ */
+
+#include <linux/bpf.h>
+
+#include "bpf_stack.h"
+#include "bpf_stack_offsets.h"
+
+static __always_inline int
+append_unicode(struct bpf_stack_args *args, void *unicode)
+{
+	unsigned int len = 0;
+	__u32 state;
+
+	if (bpf_probe_read_user(&state, sizeof(state),
+				unicode + PY_BPF_UNICODE_STATE))
+		return -EFAULT;
+
+	if (!(state & PY_BPF_UNICODE_STATE_ASCII_MASK))
+		return bpf_stack_append_char(args, '?');
+
+	return bpf_stack_append_str(args, unicode + PY_BPF_UNICODE_ASCII_DATA);
+}
+
+/*
+ * BPF-to-BPF replacement for a loader-provided bpf_stack() extension point.
+ * data is the current PyThreadState pointer supplied by the USDT handler.
+ * Write "[filename:]qualname<sep>" records from the current frame outwards.
+ * Return 0 on success or a negative errno value.
+ */
+SEC("freplace/bpf_stack")
+int
+bpf_stack(struct bpf_stack_args *args)
+{
+	void *tstate;
+	char *str;
+	unsigned short depth;
+	void *frame, *base_frame;
+	unsigned short i;
+
+	if (!args)
+		return -EINVAL;
+
+	tstate = (void *)(unsigned long)args->data;
+	str = args->str;
+	args->str_len = 0;
+	depth = args->depth;
+
+	str[0] = '\0';
+
+	if (bpf_probe_read_user(&frame, sizeof(frame),
+				 tstate + PY_BPF_TSTATE_CURRENT_FRAME) ||
+	    bpf_probe_read_user(&base_frame, sizeof(base_frame),
+				 tstate + PY_BPF_TSTATE_BASE_FRAME))
+		return -EFAULT;
+
+	if (depth > BPF_STACK_MAX_DEPTH)
+		depth = BPF_STACK_MAX_DEPTH;
+
+	for (i = 0; i < depth && frame && frame != base_frame; i++) {
+		void *previous, *code, *qualname;
+		__u64 executable;
+		int err = 0;
+
+		if (bpf_probe_read_user(&executable, sizeof(executable),
+					frame + PY_BPF_FRAME_EXECUTABLE) ||
+		    bpf_probe_read_user(&previous, sizeof(previous),
+					frame + PY_BPF_FRAME_PREVIOUS))
+			return -EFAULT;
+
+		code = (void *)(executable & ~(__u64)PY_BPF_STACKREF_REFCNT_MASK);
+		if (!code)
+			return -EFAULT;
+
+		/* For optional long display, add "filename:" */
+		if (args->flags & BPF_STACK_FLAGS_LONG) {
+			void *filename;
+
+			if (bpf_probe_read_user(&filename, sizeof(filename),
+						code + PY_BPF_CODE_FILENAME))
+				return -EFAULT;
+
+			err = append_unicode(args, filename);
+			if (!err)
+				err = bpf_stack_append_char(args, ':');
+		}
+		if (!err) {
+			if (bpf_probe_read_user(&qualname, sizeof(qualname),
+						code + PY_BPF_CODE_QUALNAME))
+				return -EFAULT;
+			err = append_unicode(args, qualname);
+		}
+		if (!err)
+			err = bpf_stack_append_char(args, args->str_sep);
+		str[args->str_len] = '\0';
+		if (err)
+			return err == -ENOSPC ? 0 : err;
+		frame = previous;
+	}
+	return 0;
+}
+
+char py_stack_license[] SEC("license") = "GPL";
---
+++ Python/bpf/bpf_stack.h
@@ -0,0 +1,84 @@
+#ifndef __BPF_STACK_H__
+#define __BPF_STACK_H__
+
+
+/* BPF declarations */
+
+struct bpf_stack_args {
+	unsigned long data;
+	unsigned int flags;
+	unsigned char depth;
+	char str_sep;
+	unsigned char  str_len;
+	unsigned char __pad;
+	char str[256];
+};
+
+enum {
+        BPF_STACK_FLAGS_LONG = 0x1,
+};
+
+#define BPF_STACK_MAX_DEPTH  16
+#define BPF_STACK_MAX_CHUNK  64
+
+#ifdef __bpf__
+
+#ifndef __BPF_HELPERS__
+static long (*bpf_probe_read_user)(void *dst, __u32 size, const void *unsafe_ptr) =
+        (void *)(long)BPF_FUNC_probe_read_user;
+static long (*bpf_probe_read_user_str)(void *dst, __u32 size, const void *unsafe_ptr) =
+        (void *)(long)BPF_FUNC_probe_read_user_str;
+#endif /* __BPF_HELPERS__ */
+
+#ifndef SEC
+#define SEC(NAME) __attribute__((section(NAME), used))
+#endif
+
+#ifndef EFAULT
+#define EFAULT 14
+#endif
+#ifndef ENOSPC
+#define ENOSPC 28
+#endif
+#ifndef EINVAL
+#define EINVAL 22
+#endif
+
+static __always_inline int
+bpf_stack_append_char(struct bpf_stack_args *args, char ch)
+{
+	if (!args)
+		return -EINVAL;
+
+	if (args->str_len + 1 >= sizeof(args->str))
+		return -ENOSPC;
+
+	args->str[args->str_len++] = ch;
+	return 0;
+}
+
+static __always_inline int
+bpf_stack_append_str(struct bpf_stack_args *args, const char *str)
+{
+	int ret;
+
+	if (!args)
+		return -EINVAL;
+
+	/* verifier */
+	if (args->str_len >= sizeof(args->str) - BPF_STACK_MAX_CHUNK - 1)
+		return -ENOSPC;
+	ret = bpf_probe_read_user_str(args->str + args->str_len,
+				      BPF_STACK_MAX_CHUNK, str);
+	if (ret < 0)
+		return ret;
+	if (ret > 0) {
+		args->str_len += ret - 1;
+		args->str[args->str_len] = '\0';
+	}
+
+	return 0;
+}
+#endif /* __bpf__ */
+
+#endif /* __BPF_STACK_H__ */
