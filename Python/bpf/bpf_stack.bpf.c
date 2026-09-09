0d8a844f0ee7024d9dff4363e3355531362492b7
Alan Maguire <maguire.alan.e@gmail.com>
2026-09-09 08:45:16 +0100

    fix hdrs

--- Python/bpf/bpf_stack.bpf.c
+++ Python/bpf/bpf_stack.bpf.c
@@ -8,47 +8,13 @@
 #include "bpf_stack.h"
 #include "bpf_stack_offsets.h"
 
-static long (*bpf_probe_read_user)(void *dst, __u32 size, const void *unsafe_ptr) =
-        (void *)(long)BPF_FUNC_probe_read_user;
-static long (*bpf_probe_read_user_str)(void *dst, __u32 size, const void *unsafe_ptr) =
-        (void *)(long)BPF_FUNC_probe_read_user_str;
-
-
-#define SEC(NAME) __attribute__((section(NAME), used))
-#define EFAULT 14
-#define ENOSPC 28
-#define EINVAL 22
-
-typedef __SIZE_TYPE__ size_t;
-
-#define PY_BPF_STACK_MAX_DEPTH	16
-#define PY_BPF_STACK_MAX_CHUNK	64
-
-static __always_inline int
-append_char(struct bpf_stack_args *args, char ch)
-{
-	if (!args)
-		return -EINVAL;
-
-	if (args->str_len + 1 >= sizeof(args->str))
-		return -ENOSPC;
-
-	args->str[args->str_len++] = ch;
-	return 0;
-}
-
 static __always_inline int
 append_unicode(struct bpf_stack_args *args, void *unicode)
 {
 	int nospace = 0;
-	size_t count;
 	__s64 length;
 	__u32 state;
-	char *str;
-	int ret;
-
-	if (!args)
-		return -EINVAL;
+	size_t len;
 
 	if (bpf_probe_read_user(&length, sizeof(length),
 				unicode + PY_BPF_UNICODE_LENGTH) ||
@@ -57,28 +23,14 @@ append_unicode(struct bpf_stack_args *args, void *unicode)
 		return -EFAULT;
 
 	if (length < 0 || !(state & PY_BPF_UNICODE_STATE_ASCII_MASK))
-		return append_char(args, '?');
+		return bpf_stack_append_char(args, '?');
 
-	count = (size_t)length;
-	if (count == 0)
+	len = (size_t)length;
+	if (len == 0)
 		return 0;
 
-	/* verifier */
-	str = args->str + (args->str_len & (sizeof(args->str) - 1));
-
-	if (PY_BPF_STACK_MAX_CHUNK > sizeof(args->str) - args->str_len)
-		return -ENOSPC;
-	ret = bpf_probe_read_user_str(str, PY_BPF_STACK_MAX_CHUNK,
-				      unicode + PY_BPF_UNICODE_ASCII_DATA);
-	if (ret < 0)
-		return ret;
-
-	if (count > PY_BPF_STACK_MAX_CHUNK)
-		args->str_len += PY_BPF_STACK_MAX_CHUNK;
-	else
-		args->str_len += count;
-
-	return nospace ? -ENOSPC : 0;
+	return bpf_stack_append_str(args, unicode + PY_BPF_UNICODE_ASCII_DATA,
+				    len);
 }
 
 /*
@@ -113,8 +65,8 @@ bpf_stack(struct bpf_stack_args *args)
 				 tstate + PY_BPF_TSTATE_BASE_FRAME))
 		return -EFAULT;
 
-	if (depth > PY_BPF_STACK_MAX_DEPTH)
-		depth = PY_BPF_STACK_MAX_DEPTH;
+	if (depth > BPF_STACK_MAX_DEPTH)
+		depth = BPF_STACK_MAX_DEPTH;
 
 	for (i = 0; i < depth && frame && frame != base_frame; i++) {
 		void *previous, *code, *qualname;
@@ -141,7 +93,7 @@ bpf_stack(struct bpf_stack_args *args)
 
 			err = append_unicode(args, filename);
 			if (!err)
-				err = append_char(args, ':');
+				err = bpf_stack_append_char(args, ':');
 		}
 		if (!err) {
 			if (bpf_probe_read_user(&qualname, sizeof(qualname),
@@ -150,7 +102,7 @@ bpf_stack(struct bpf_stack_args *args)
 			err = append_unicode(args, qualname);
 		}
 		if (!err)
-			err = append_char(args, args->str_sep);
+			err = bpf_stack_append_char(args, args->str_sep);
 		str[args->str_len] = '\0';
 		if (err)
 			return err == -ENOSPC ? 0 : err;
--- Python/bpf/bpf_stack.h
+++ Python/bpf/bpf_stack.h
@@ -18,4 +18,75 @@ enum {
         BPF_STACK_FLAGS_LONG = 0x1,
 };
 
+#define BPF_STACK_MAX_DEPTH  16
+#define BPF_STACK_MAX_CHUNK  64
+
+#ifdef __bpf__
+
+#ifndef __BPF_HELPERS__
+static long (*bpf_probe_read_user)(void *dst, __u32 size, const void *unsafe_ptr) =
+        (void *)(long)BPF_FUNC_probe_read_user;
+static long (*bpf_probe_read_user_str)(void *dst, __u32 size, const void *unsafe_ptr) =
+        (void *)(long)BPF_FUNC_probe_read_user_str;
+#endif /* __BPF_HELPERS__ */
+
+#ifndef SEC
+#define SEC(NAME) __attribute__((section(NAME), used))
+#endif
+
+#ifndef EFAULT
+#define EFAULT 14
+#endif
+#ifndef ENOSPC
+#define ENOSPC 28
+#endif
+#ifndef EINVAL
+#define EINVAL 22
+#endif
+
+typedef __SIZE_TYPE__ size_t;
+
+static __always_inline int
+bpf_stack_append_char(struct bpf_stack_args *args, char ch)
+{
+	if (!args)
+		return -EINVAL;
+
+	if (args->str_len + 1 >= sizeof(args->str))
+		return -ENOSPC;
+
+	args->str[args->str_len++] = ch;
+	return 0;
+}
+
+static __always_inline int
+bpf_stack_append_str(struct bpf_stack_args *args, const char *str, size_t len)
+{
+	char *end;
+	int ret;
+
+	if (!args)
+		return -EINVAL;
+
+	if (len == 0)
+		return 0;
+
+	/* verifier */
+	end = args->str + (args->str_len & (sizeof(args->str) - 1));
+
+	if (BPF_STACK_MAX_CHUNK > sizeof(args->str) - args->str_len)
+		return -ENOSPC;
+	ret = bpf_probe_read_user_str(end, BPF_STACK_MAX_CHUNK, str);
+	if (ret < 0)
+		return ret;
+
+	if (len > BPF_STACK_MAX_CHUNK)
+		args->str_len += BPF_STACK_MAX_CHUNK;
+        else
+                args->str_len += len;
+
+	return 0;
+}
+#endif /* __bpf__ */
+
 #endif /* __BPP_STACK_H__ */
